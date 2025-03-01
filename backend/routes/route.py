d6adcc24919d0ebd26200b1578610c73b73d0004
Yujin <wjd2000db@gmail.com>
2025-03-01 18:13:55 -0500

    add api calls for user

--- backend/routes/route.py
+++ backend/routes/route.py
@@ -11,3 +11,18 @@ router = APIRouter()
 async def get_users():
   users = list_serial(collection_name.find())
   return users
+
+#POST Request Method
+@router.post("/")
+async def post_user(user: User):
+  collection_name.insert_one(dict(user))
+
+#PUT Request Method
+@router.put("/{id}")
+async def put_user(id: str, user: User):
+  collection_name.find_one_and_update({"_id": ObjectId(id)},{"$set": dict(user)})
+
+#DELETE Request Method
+@router.delete("/{id}")
+async def delete_user(id:str):
+  collection_name.find_one_and_delete({"_id":ObjectId(id)})
