82a620b12c3d20b011930043b32351dad93d8ba8
Yujin <wjd2000db@gmail.com>
2025-02-28 23:59:15 -0500

    fix tailwind error and setup firebase

--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,28 @@
 import { Text, View } from "react-native";
 import '../global.css'
+import { useState, useEffect } from "react";
+import { initializeApp } from "firebase/app";
 
+// Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyBUDGgwxh9E2rPfxUw_o4X-Qcbbb6L8TdE",
+  authDomain: "conhacks-47acb.firebaseapp.com",
+  projectId: "conhacks-47acb",
+  storageBucket: "conhacks-47acb.firebasestorage.app",
+  messagingSenderId: "211651582321",
+  appId: "1:211651582321:web:7da6f0526a39598f5cfed0"
+};
+
+const app = initializeApp(firebaseConfig);
 
 export default function Index() {
+
+
   return (
-    <View className="flex-1 justify-center items-center">
-      <Text className="text-white text-lg">Welcome to Conhacks!</Text>
+    <View className="flex-1 justify-center items-center bg-gray-900">
+      <View className="w-50 h-10 bg-blue-500 justify-center items-center">
+        <Text className="text-white text-lg">Welcome to Conhacks!</Text>
+      </View>
     </View>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,7 @@ module.exports = {
   content: [
     './app/**/*.{js,tsx,ts,jsx}',
   ],
+  presets: [require("nativewind/preset")],
   theme: {
     extend: {},
   },
