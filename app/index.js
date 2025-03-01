d29ab49b33e1271f8189b1cbc97adde72c3288f1
Yujin <wjd2000db@gmail.com>
2025-03-01 02:24:40 -0500

    remove tailwind/nativewind css

--- app/index.js
+++ app/index.js
@@ -3,7 +3,7 @@ import { View, Text } from 'react-native';
 export default function Index() {
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Index page</Text>
+      <Text>Restart the app</Text>
     </View>
   );
 }
